30f5d035bffe8bc25de9b154b9e0b2f68ca02108
EhoodGoren <ehoodgoren@gmail.com>
2021-12-03 13:48:41 +0200

    Added error handler.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const errorHandler = require('./backend/middlewares/errorHandler');
 
 const app = express();
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 })
+
+app.use(express.json());
+
+app.use(errorHandler);
