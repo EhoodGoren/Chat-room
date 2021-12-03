c158ff5ce1a8ada2edec36d8a324fe1a87ef6e1a
EhoodGoren <ehoodgoren@gmail.com>
2021-12-03 17:50:34 +0200

    Created basic client template to work with

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Chat.css';
 
 export default function Chat(props) {
     return (
-        <div>Chat</div>
+        <div id='chat-screen'>
+            <div id='messages' className='chat-components'>
+                <input id='user-message'></input>
+                <button id='send-message'>Send</button>
+            </div>
+            <div id='participants' className='chat-components'></div>
+            <div id='message-write' className='chat-components'></div>
+        </div>
     )
 }
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function Login(props) {
+    const inputEl = useRef(null);
+    const submitUsername = () => {
+        console.log(inputEl.current.value);
+    }
     return (
-        <div>Login</div>
+        <div id='login-form'>
+            <input ref={inputEl} id='username' placeholder='Enter username'></input>
+            <button id='submit-username' onClick={submitUsername}>Submit</button>
+        </div>
     )
 }
