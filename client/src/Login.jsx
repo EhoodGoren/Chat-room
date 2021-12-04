2fe2f013bf3a6faaaac037620639441bd8ce645a
EhoodGoren <ehoodgoren@gmail.com>
2021-12-04 22:09:11 +0200

    Added page navigation after login.
    User name carries over to the chat page from state

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,8 +1,10 @@
 import React, { useRef } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Login(props) {
+    const navigate = useNavigate();
     const inputEl = useRef(null);
     const submitUsername = async () => {
         const username = inputEl.current.value;
@@ -10,7 +12,7 @@ export default function Login(props) {
         try {
             await axios.post('/login', {user: inputEl.current.value});
             props.setUser(username);
-            window.location='http://localhost:3000/chat';
+            navigate('/chat');
         } catch (error) {
             console.log(error);
         }
